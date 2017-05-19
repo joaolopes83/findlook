777db7c7f7fd00e326ae45569b498d1ad6b741e5
joaolopes83 <jocalopes83@hotmail.com>
2017-05-20 00:20:26 +0100

    commit

--- aplicacao/gruntfile.js
+++ aplicacao/gruntfile.js
@@ -1,22 +1,12 @@
 module.exports=function(grunt){
 
 "use strict";
+
+
 var config;
 config={
     //Imagemin
     imagemin: {                          // Task
-    static: {                          // Target
-      options: {                       // Target options
-        optimizationLevel: 3,
-        svgoPlugins: [{ removeViewBox: false }],
-        use: [mozjpeg()]
-      },
-      files: {                         // Dictionary of files
-        'dist/img.png': 'src/img.png', // 'destination': 'source'
-        'dist/img.jpg': 'src/img.jpg',
-        'dist/img.gif': 'src/img.gif'
-      }
-    },
     dynamic: {                         // Another target
       files: [{
         expand: true,                  // Enable dynamic expansion
