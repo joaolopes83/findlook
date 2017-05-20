417e3790d7852a8b58ef09f460eec36f6e2cb622
joaolopes83 <jocalopes83@hotmail.com>
2017-05-20 17:37:17 +0100

    versao_19-5-2017

---
+++ aplicacao/modelo/postite.js
@@ -0,0 +1,8 @@
+var postite = (function () {
+    function postite(titulo, conteudo, data) {
+        this.titulo_postite = titulo;
+        this.conteudo_postite = conteudo;
+        this.data_postite = data;
+    }
+    return postite;
+})();
---
+++ aplicacao/modelo/postite.ts
@@ -0,0 +1,13 @@
+class postite{
+private titulo_postite:string;
+private conteudo_postite:string;
+private data_postite:Date;
+constructor(titulo:string, conteudo:string,data:Date){
+this.titulo_postite=titulo;
+this.conteudo_postite=conteudo;
+this.data_postite=data;
+
+}
+
+
+}
